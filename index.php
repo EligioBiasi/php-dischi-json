12e615b300ad10b5e7d6586e4861bc19bda9eeae
EligioBiasi <biasi.eligio@edufaimm.it>
2023-07-05 15:37:48 +0200

    added header

--- index.php
+++ index.php
@@ -4,9 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php-dischi-json</title>
+    <!-- Bootstrap 5.2.3 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <!-- my style -->
     <link rel="stylesheet" href="./styles/style.css">
+    <!-- axios 1.4.0 -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.4.0/axios.min.js"></script>
+    <!-- vue 3 -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+
 </head>
     <body>
-        
+        <div id="app">
+            <header>
+                <div class="container">
+                    <img src="./images/Spotify_icon.svg.png" alt="Spotify Logo" id="logo" class="py-3">
+                </div>
+            </header>
+                
+            
+            
+        </div>
+
+        <script src="./database/main.js"></script>
     </body>
 </html>
